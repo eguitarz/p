b8b9318d13fd5b205f561611392216964c4c079f
Dale Ma <dalema22@gmail.com>
2017-05-08 21:51:49 -0700

    add child pages

--- src/ui/components/eguitarz-github-com/component.ts
+++ src/ui/components/eguitarz-github-com/component.ts
@@ -1,5 +1,14 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class EguitarzGithubCom extends Component {
+  @tracked page = 'about-me';
 
+  @tracked('page')
+  get isHome() {
+    return this.page === 'about-me';
+  }
+
+  select(page) {
+    this.page = page;
+  }
 }
